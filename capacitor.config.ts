598fa5ef36b35b1edc81132cb9090b4f2692338d
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-16 10:00:30 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobizen_sms',
+  webDir: 'www'
+};
+
+export default config;
