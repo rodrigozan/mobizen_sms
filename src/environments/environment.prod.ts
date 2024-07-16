a927a82f7c6cd327c6b2bd931f2bc0284e063032
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-16 11:53:06 -0300

    Created environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  api_mobizen_key: 'SEU_TOKEN_MOBIZEN',
+  api_mobizen_url: 'https://api.mobizon.com.br',
+  api_mobizen_version: 'v1',
+  api_mobizen_format: 'json'
 };
