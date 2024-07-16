087b0eb2b6d744cd2a2d60038ab60d1d56fd467c
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-16 12:13:37 -0300

    Created sendSms method and import dependencies

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonTextarea, IonButton } from '@ionic/angular/standalone';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+import { SmsService } from '../../services/sms.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [FormsModule, IonButton, IonTextarea, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+
+  recipients: string = '';
+  text: string = '';
+  results: string[] = [];
+
+  constructor(private smsService: SmsService) {}
+
+  async sendSms() {
+    this.results = ['Enviando SMS...'];
+    const recipientList = this.recipients.split(',').map(recipient => recipient.trim());
+    try {
+      this.results = await this.smsService.sendSms(recipientList, this.text);
+    } catch (error) {
+      this.results = [`Erro ao enviar SMS: ${error.message}`];
+    }
+  }
 }
